dfa649924ba24a40ba594435e85db4cb53278403
Robert Prado <robert.prado@hubchain.io>
2020-02-16 14:47:40 -0300

    Adicionado o primeiro arquivo Js

---
+++ scripts.js
@@ -0,0 +1,12 @@
+function mostrargreen() {
+    console.log("funçao green foi criada");
+    
+
+    
+}
+mostrargreen()
+console.log("eu aprendi a criar, chamar e executar uma funçao");
+console.log("primeiro passo ao criar uma funçao: function nomedado");
+console.log("segundo passo: criar o meu codigo, console.log");
+console.log("terceiro passo: chamar a minha função")
+
