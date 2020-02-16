de0366929af2de13ce8c5ddcbb6b30925037559b
Alexia Raphaela <alexiaraphaela@gmail.com>
2020-02-16 18:32:52 -0300

    Adicionado o primeiro arquivo JS

---
+++ scripts.js
@@ -0,0 +1,8 @@
+// criei um bloco de função
+function mostrarGreen() {
+    // Chamei uma função que já existia 
+    console.log("A função mostrarGreen() foi chamada");
+}
+
+// Chamei a função mostrarGreen() que eu criei
+mostrarGreen()
